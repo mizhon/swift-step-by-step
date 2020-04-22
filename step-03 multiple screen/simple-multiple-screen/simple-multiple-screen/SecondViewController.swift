936aecc257ae432cca71d8a29c47295783cb25e6
mizhon <mizhon@live.cn>
2020-04-22 22:05:56 +0800

    添加简单的swift页面跳转方法

---
+++ step-03 multiple screen/simple-multiple-screen/simple-multiple-screen/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  simple-multiple-screen
+//
+//  Created by zhongming on 2020/4/22.
+//  Copyright © 2020 zhongming. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ step-03 multiple screen/simple-multiple-screen/simple-multiple-screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-multiple-screen
+//
+//  Created by zhongming on 2020/4/22.
+//  Copyright © 2020 zhongming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
